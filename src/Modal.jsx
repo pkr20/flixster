85c700ba41188bf08d25310e99663dfd042170ed
Kayleen Ramirez <pkr20@miami.edu>
2025-06-12 12:59:27 -0700

    close button now added to all modals

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,10 +1,13 @@
 
 const Modal = ({movie, onClose}) => {
+    
+
     return (
         <div className='modal-overlay'>
             <div className='modal-content'>
                 <h1>{movie.title}</h1>
                 <p>{movie.description}</p>
+                <button onClick={onClose}>Close</button>
 
             </div>
         </div>
